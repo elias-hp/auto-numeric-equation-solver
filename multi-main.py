0532d8e25ccb1bfcb12ca1c27bb4eb21385304e7
Elias Hormazábal Pernler <elias.hp@protonmail.com>
2021-12-17 22:23:50 +0100

    starts multiple processes & gives solutions

--- multi-main.py
+++ multi-main.py
@@ -3,6 +3,8 @@ import sqlite3
 import logging
 import queue
 import multiprocessing
+from configparser import ConfigParser
+
 
 # TODO: Documentation
 # hur funktioner beskrivs:
@@ -10,6 +12,21 @@ import multiprocessing
 # termer => (5, -3)
 # t0 => koefficienten för termen, t1 => variabeln, t2 => exponenten till variabeln
 # funktion: (5, -3) + (7, 1) + (-19, 0)
+
+# construct polynomial function from configuration file string
+# TODO: Shorten
+def construct_polynomial(polynomial_str: str) -> list:
+    # format raw list w. string tuples
+    raw_list = list(polynomial_str.strip('[]').split('\\'))
+
+    # format tuples of koefficient & exponent
+    polynomial = []
+    for t in raw_list:
+        raw_t = eval(t)
+        polynomial.append((float(raw_t[0]), float(raw_t[1])))
+
+    return polynomial
+
 
 # funktion som deriverar
 # TODO: Shorten
@@ -27,6 +44,7 @@ def derivera(function: list):
 
     # print(f'deriverad funk: {derv_funktion}')
     return derv_funktion
+
 
 # TODO: Shorten
 def kalk_funk(funktion: list, x):
@@ -48,66 +66,132 @@ def kalk_funk(funktion: list, x):
 # TODO: Recognize already found solutions & skip logg/save
 # TODO: Recognize already searched areas
 # TODO: Recognize max solutions & end algorithm when done
-def algoritm(funktion:list, noggrannhet:int, startvärde:float):
-
+# TODO: Handle polynomials w.o. solutions
+def algoritm(funktion: list, noggrannhet: int, startvärde: float, Que: queue.Queue):
     x = startvärde
     old_res = None
     while True:
 
+        # try numerical equation
         try:
             resultat = x - (kalk_funk(funktion=funktion, x=x) / kalk_funk(derivera(funktion), x))
-        except ZeroDivisionError:
+        except ZeroDivisionError:  # division by zero => extreme value
             print(f'Hittade extrempunkt, X = {x}')
             print(f'Proces terminerar sig själv!')
             exit()
 
+        # first run of algorithm
         if not old_res:
             x = resultat
             old_res = resultat
-        elif round(old_res, noggrannhet) == round(resultat, noggrannhet):
-            print(f'Hittade nollställe: X = {resultat}')
-            break
-        else:
 
+        # if found solution
+        elif round(old_res, noggrannhet) == round(resultat, noggrannhet):
+            Que.put(resultat)
+            break
+
+        # no passable solution found
+        else:
             x = resultat
             old_res = resultat
             # print(f'X = {resultat}')
 
 
+# TODO: Print all solutions
+# TODO: Terminate algorithms when all solutions found
+def communicator(Que: queue.Queue, polynomial: list, prcs_list: list):
+    solutions = []
+
+    # Determine max solutions
+    max_solutions = max(polynomial, key=lambda item: item[1])[1]
+
+    # active role
+    while True:
+
+        # kill program when all solutions found
+        if len(solutions) == max_solutions:
+            print(f"all found {solutions}")
+            for prcs in prcs_list:
+                prcs.kill()
+            break
+
+        # handle new solutions
+        elif not Que.empty():
+            new_x = Que.get()
+            found = False
+
+            # search if solution already saved
+            for x in solutions:
+                if x == new_x:
+                    found = True
+
+            # save new solution
+            if not found:
+                solutions.append(new_x)
+
+    print(f'All solutions found, X = {solutions}')
+
+
 def main():
+    # # Settings & function-construction
+    if input('Use configuration-file? y/n ') == 'y':
 
-    # # meny för funktionsinmatning
-    # konstruera funktion
-    # TODO: Add option for configuration file, fast testing
-    gradtal = int(input("Ekvationens gradtal: "))
+        # Read config file & apply settings
+        config = ConfigParser()
+        config.read('config.ini')
 
-    funktion = []
+        # construct function
+        funktion = construct_polynomial(config.get('SETTINGS', 'polynomial'))
+        marginal = int(config.get('SETTINGS', 'margin'))
+        prcs_max = int(config.get('SETTINGS', 'prcs_max'))
+        noggrannhet = int(config.get('SETTINGS', 'accuracy'))
 
-    for i in range(gradtal):
-        k = float(input(f"Koefficientet till x^{gradtal - i}: "))
-        funktion.append((k, gradtal - i))
+    else:
+        # meny för funktionsinmatning
+        # konstruera funktion
+        gradtal = int(input("Ekvationens gradtal: "))
 
-    funktion.append((float(input('Konstant: ')), 0))
+        funktion = []
 
-    # marginal & inställningar
-    marginal = int(input('Absolut marginal: '))
+        for i in range(gradtal):
+            k = float(input(f"Koefficientet till x^{gradtal - i}: "))
+            funktion.append((k, gradtal - i))
 
-    prcs_max = int(input('Maximalt jämnt-antal processer: '))
+        funktion.append((float(input('Konstant: ')), 0))
 
-    noggrannhet = int(input('Noggrannhet, antal decimaler: '))
+        # marginal & inställningar
+        marginal = int(input('Marginal (positiv X): '))
+
+        prcs_max = int(input('Maximalt jämnt-antal processer: '))
+
+        noggrannhet = int(input('Noggrannhet, antal decimaler: '))
+
+    # Queue
+    Que = multiprocessing.Queue()
 
     # # multiprocesser
     # TODO: Analyse area in which there could be a solution
-    for i in range(int(prcs_max/2)):
+    prcs_list = []
+    for i in range(prcs_max):
+        prcs1 = multiprocessing.Process(target=algoritm,
+                                        args=(funktion, noggrannhet, ((-marginal / (2 * prcs_max)) * (i + 1)), Que))
+        prcs2 = multiprocessing.Process(target=algoritm,
+                                        args=(funktion, noggrannhet, ((marginal / (2 * prcs_max)) * (i + 1)), Que))
+        prcs_list.append(prcs1)
+        prcs_list.append(prcs2)
 
-        prcs1 = multiprocessing.Process(target=algoritm, args=(funktion, noggrannhet, ((-marginal/(2*prcs_max))*(i+1))))
-        prcs2 = multiprocessing.Process(target=algoritm, args=(funktion, noggrannhet, ((marginal/(2*prcs_max))*(i+1))))
+    # start communicator process
+    comm = multiprocessing.Process(target=communicator, args=(Que, funktion, prcs_list))
+    comm.start()
 
-        prcs1.start()
-        prcs2.start()
+    # start all processes
+    for j, prcs in enumerate(prcs_list):
+        prcs.start()
 
-    prcs1.join()
-    prcs2.join()
+    # join all processes
+    for prcs in prcs_list:
+        prcs.join()
+        comm.join()
 
     # TODO: Add tests
 
